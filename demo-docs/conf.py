cc152be249be1d3ffa4ecc64980534401a04bf1a
shekharrajak <shekharstudy@ymail.com>
2019-09-07 20:30:36 +0530

    sidebar right added - toc and html file formatting

--- demo-docs/conf.py
+++ demo-docs/conf.py
@@ -17,8 +17,8 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
 project = u'scipy-sphinx-theme-v2'
-version = '0.1'
-release = '0.1'
+version = '0.2'
+release = '0.2'
 exclude_patterns = ['_build']
 pygments_style = 'sphinx'
 
